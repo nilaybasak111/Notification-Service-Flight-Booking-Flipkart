de4a11b6483f350c403f862070a7c811624d864d
Nilay Basak <nilaybasak000@gmail.com>
2025-05-20 14:31:39 +0530

    Created NodeMailer Transport Object & Tested

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ const express = require('express');
 const { ServerConfig } = require('./config');
 const apiRoutes = require('./routes');
 
+const mailsender = require('./config/email-config');
 const app = express();
 
 app.use(express.json());
@@ -10,6 +11,17 @@ app.use(express.urlencoded({extended: true}));
 
 app.use('/api', apiRoutes);
 
-app.listen(ServerConfig.PORT, () => {
+app.listen(ServerConfig.PORT, async () => {
     console.log(`Successfully started the server on PORT : ${ServerConfig.PORT}`);
+    try{
+        const response= await mailsender.sendMail({
+        from: ServerConfig.GMAIL_EMAIL,
+        to: "ke.i.r.an.iangl.yt@gmail.com",
+        subject: "Service Working Fine 2",
+        text: "The service is up and running 2",
+    });
+    console.log("Email sent successfully", response);
+    } catch (error) {
+        console.error("Error sending email", error);
+    }
 });
---
+++ src/utils/common/enums.js
@@ -0,0 +1,7 @@
+const CONFIRMATION_STATUS = {
+  PENDING: "pending",
+  SUCCESS: "success",
+  FAILED: "failed",
+};
+
+module.exports = { CONFIRMATION_STATUS };
